1cf41c92b240e1d7167f917a22ec41bf2bc08c92
freechecklistapp <kigkoca@gmail.com>
2024-05-13 15:45:06 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,34 +1,18 @@
-importScripts('https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.11.1/firebase-messaging.js');
+import { getMessaging, getToken } from "firebase/messaging";
 
-// Initialize the Firebase app in the service worker by passing in the Firebase config object
-const firebaseConfig = {
-  apiKey: "AIzaSyDjR8b_DUOKhvd9CP2ymeFdJ2k_EYD0ua0",
-  authDomain: "free-checklist-app.firebaseapp.com",
-  projectId: "free-checklist-app",
-  storageBucket: "free-checklist-app.appspot.com",
-  messagingSenderId: "43846520306",
-  appId: "1:43846520306:web:ecca6466a9573f4145ef6b",
-  measurementId: "G-2M1TMN1DDH"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background messages
-const messaging = firebase.messaging();
-
-// Add an event listener to handle background messages
-self.addEventListener('push', function(event) {
-  const payload = event.data.json();
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon
-    // You can add more options as needed
-  };
-
-  // Show the notification
-  event.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+getToken(messaging, { vapidKey: 'BNBePJ1gN2-7q4ic8fgTwdxoGMTySApYQ5N26vUv1__Ts16Jc1P3-4FqIr3_dtnxorXeLaHscs0NHSunVILYIwQ' }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
 });
-
-
