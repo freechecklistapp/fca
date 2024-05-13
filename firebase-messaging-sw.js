ed04240b143bbb8d99ff71a6c0c9e8f1f56067f1
freechecklistapp <kigkoca@gmail.com>
2024-05-13 16:21:41 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,9 @@
+// Load Firebase SDKs
 importScripts('https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/10.11.1/firebase-messaging.js');
 
-// Initialize Firebase app in the service worker
-const firebaseConfig = {
+// Initialize Firebase app
+firebase.initializeApp({
   apiKey: "AIzaSyDjR8b_DUOKhvd9CP2ymeFdJ2k_EYD0ua0",
   authDomain: "free-checklist-app.firebaseapp.com",
   projectId: "free-checklist-app",
@@ -10,14 +11,13 @@ const firebaseConfig = {
   messagingSenderId: "43846520306",
   appId: "1:43846520306:web:ecca6466a9573f4145ef6b",
   measurementId: "G-2M1TMN1DDH"
-};
-firebase.initializeApp(firebaseConfig);
+});
 
 // Retrieve Firebase Messaging instance
 const messaging = firebase.messaging();
 
 // Retrieve registration token
-messaging.getToken({ vapidKey: 'BNBePJ1gN2-7q4ic8fgTwdxoGMTySApYQ5N26vUv1__Ts16Jc1P3-4FqIr3_dtnxorXeLaHscs0NHSunVILYIwQ&#39' }).then((currentToken) => {
+messaging.getToken({ vapidKey: 'YOUR_VAPID_KEY' }).then((currentToken) => {
   if (currentToken) {
     // Send the token to your server and update the UI if necessary
     console.log('Registration token:', currentToken);
@@ -28,4 +28,3 @@ messaging.getToken({ vapidKey: 'BNBePJ1gN2-7q4ic8fgTwdxoGMTySApYQ5N26vUv1__Ts16J
 }).catch((err) => {
   console.error('An error occurred while retrieving token:', err);
 });
-
